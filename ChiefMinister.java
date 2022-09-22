8c8d704d5c3dc4a9c5eaff09f0baf10b21edb726
tilakgowdaa <tilakgowda.xworkz@gmail.com>
2022-09-22 16:02:45 +0530

    MADIDINI

---
+++ ChiefMinister.java
@@ -0,0 +1,20 @@
+class ChiefMinister{
+	public static void main(String[] minister)
+	{
+		
+		System.out.println("Information of Chief Minister");
+		
+		
+		System.out.println("name:"+minister[0]);
+		System.out.println("age:"+minister[1]);
+		System.out.println("homeTown:"+minister[2]);
+		System.out.println("fatherName:"+minister[3]);
+		System.out.println("motherName:"+minister[4]);
+		System.out.println("partyName:"+minister[5]);
+		System.out.println("constituency:"+minister[6]);
+		System.out.println("networth:"+minister[7]);
+		System.out.println("qualification:"+minister[8]);
+		System.out.println("contribution:"+minister[9]);
+	}
+	
+}
